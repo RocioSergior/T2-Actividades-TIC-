308491c88737fb4e158bc7813ad1e1e191f1246d
rgarbel1710 <151036654+RocioSergior@users.noreply.github.com>
2024-02-23 20:07:19 +0100

    Update script.js

--- actividad 6/js/script.js
+++ actividad 6/js/script.js
@@ -1,13 +1,9 @@
 function comprobar() {
 	let numero = parseInt(document.getElementById("numero").value,10);
 	if(numero%2==0){
-	  //Mensaje si el numero divididom es  tiene resto 1.
 	  document.getElementById("texto").innerHTML="el numero es par";
-	  console.log("par")
 	}
 	else{
-	  //Mensaje si el numero dividido es tiene resto 0
 	 document.getElementById("texto").innerHTML="el numero es impar";
-	 console.log("impar")
 	}
  }
